8042a3d3854cde740d9bafbdede317e728d43890
jcalero6212 <107775341+jcalero6212@users.noreply.github.com>
2025-04-17 14:57:28 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClass
+{
+    public class Producto
+
+    {
+
+        // Campos privados 
+
+        private string nombre;
+
+        private decimal precio;
+
+
+
+        // Propiedades públicas 
+
+        public string Nombre
+
+        {
+
+            get { return nombre; }
+
+            set { nombre = value; }
+
+        }
+
+
+
+        public decimal Precio
+
+        {
+
+            get { return precio; }
+
+            set { precio = value; }
+
+        }
+
+
+
+        // Método que retorna descripción 
+
+        public string ObtenerDescripcion()
+
+        {
+
+            return $"Producto: {Nombre}, Precio: {Precio:C}";
+
+        }
+
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("BibliotecaEjemplo")]
+[assembly: AssemblyTitle("BibliotecaClass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BibliotecaEjemplo")]
+[assembly: AssemblyProduct("BibliotecaClass")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("79266a0e-c9cf-434b-a041-1898b27e405f")]
+[assembly: Guid("df297a46-6965-4425-a087-8de9d01597f8")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
